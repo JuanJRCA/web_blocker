93407eea39a865cef024a889257f9640d80087ed
juanrubio <juanrubiodatascience@gmail.com>
2020-01-27 23:13:27 +0100

    clean code in gui.py, add private functions, put documentation in english, improve attributes and variables names

--- gui.py
+++ gui.py
@@ -1,54 +1,73 @@
-import tkinter
 import os
-
-
+import tkinter
 class Gui():
-    '''Sencilla interfaz gráfica. Se inicializa con una pantalla de tkinter.'''
+
+    '''Graphical interface made with Tkinter. The websites introduced by the
+    user is saved and written in other files to block the website.'''
+
     def __init__(self, root):
+
+        ''' Initialize the path of the hosts, the redirection value, and the
+        path where the websites blocked will be saved. Tkinter is launched. '''
+
         self.redirection = "127.0.0.1"
-        self.path = str(os.path.abspath("blocked_urls.txt"))
+        absolute_path = os.path.abspath("blocked_urls.txt")
+        self.path_save_blocked_websites = str(absolute_path)
         self.hosts = r"/etc/hosts"
-        self.data = []
+        self.data = None
 
-        startLabel = tkinter.Label(root, text="Introduce la web que deseas bloquear: ")
-        startLabel.pack()
-        self.startEntry = tkinter.Entry(root)
-        self.startEntry.pack()
-        self.original = self.startEntry
+        start_msg = "Introduce la web que deseas bloquear:"
+        start_label = tkinter.Label(root, text=start_msg)
+        start_label.pack()
 
-        plotButton = tkinter.Button(root, text="Confirmar", command=self.input_data)
-        plotButton.pack()
+        self.start_entry = tkinter.Entry(root)
+        self.start_entry.pack()
 
-    def escribir_documento_txt(self):
-        ''' Escribe en el documento txt y en hosts la web introducida.'''
-        with open(self.path, "r+") as documentoWebs:
-            webs = documentoWebs.read()
-            for x in self.data:
-                if x in webs:
-                    pass
-                else:
-                    documentoWebs.write(x+"\n")
-            documentoWebs.close()
+        plot_button = tkinter.Button(root,
+                                     text="Confirmar",
+                                     command=self.input_data)
+        plot_button.pack()
 
-        with open(self.hosts, "r+") as hosts:
-            websBlocked = hosts.read()
-            for x in self.data:
-                if x in websBlocked:
-                    pass
-                else:
-                    hosts.write((self.redirection + "    " + str(x) + "\n"))
-            hosts.close()
+    def _block_save_selected_websites(self):
+
+        ''' Block the websites writing in hosts file. Also saves the website in the
+        txt document.'''
+
+        with open(self.path_save_blocked_websites, "r+") as txt_with_saved_blocked_websites:
+            self._write_in_document(txt_with_saved_blocked_websites)
+
+        with open(self.hosts, "r+") as path_to_block_websites:
+            self._write_in_document(path_to_block_websites)
+
 
     def input_data(self):
-        ''' Recoge la información introducida por el usuario una vez pinchado el button'''
-        if self.startEntry.get() == "":
+
+        ''' Recolects the data that the user wrote on the graphical interface. '''
+
+        info_in_entry = self.start_entry.get()
+        original = self.start_entry
+
+        if info_in_entry == "":
             print("No has introducido ninguna web")
-            pass
+
         else:
 
-            self.data.append(self.startEntry.get())
-            self.escribir_documento_txt()
-            print("La siguiente web ha sido bloqueada : {}".format(self.data[-1]))
+            self.data = info_in_entry
+            print("La siguiente web será bloqueada" , self.data)
+            self._block_save_selected_websites()
+            self.start_entry = self.start_entry.delete(0, 'end')
+            self.start_entry = original
+
+    def _write_in_document(self,pageRead):
+            file_read = pageRead.read()
+
+            if self.data in file_read:
+                pass
+
+            else:
+                pageRead.write(self.redirection + "    " +
+                               str(self.data) + "\n")
+
         self.startEntry = self.startEntry.delete(0, 'end')
         self.startEntry = self.original
 
--- launch.py
+++ launch.py
@@ -1,11 +1,15 @@
-import os
 import tkinter
-
 from gui import Gui
+''' 
+Launch the program. root creates the object that the Gui will use.
+Once we close the gui, the program will stop.
+
+'''
 
 root = tkinter.Tk()
 root.geometry("500x100")
 root.title("Bloquear Webs")
-gui = Gui(root)
+Gui(root)
 root.mainloop()
+
 
